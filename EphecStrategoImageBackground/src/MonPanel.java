428f588d016f8d39b2ae0216029fbae23f9482e5
mnvro <mnvro@hotmail.com>
2014-12-12 11:36:51 +0100

    Exemple de code donné par Nicolas Perivolaris (3TI) pour afficher une image en backgroud dans un JPanel.

---
+++ EphecStrategoImageBackground/src/MonPanel.java
@@ -0,0 +1,58 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+/**
+ * 
+ * @author Peri
+ *
+ */
+public class MonPanel extends JPanel {
+	
+	private static final long serialVersionUID = 7203738621706153450L;
+	private BufferedImage background;
+	
+	public MonPanel() {
+		setOpaque(false); //le background devient transparent
+		background = getImage("images/image.jpg"); //chargement de l'image
+		Dimension d = new Dimension(background.getWidth(), background.getHeight()); //récupération des dimensions de l'image
+		setPreferredSize(d); // resize la taille du panel à la taille de l'image
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		//methode appelée automatiquement par java lors du rendering du JPanel
+		g.drawImage(background, 0, 0, this); 
+		super.paintComponent(g); 
+	}
+	
+	/**
+	 * Chargement de l'image à partir du disque
+	 * @param path le chemin vers l'image
+	 * @return l'image si elle est trouvée sur le disque, null sinon
+	 */
+	private BufferedImage getImage(String path){
+		try {
+			return ImageIO.read(new File(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.add(new MonPanel(), BorderLayout.CENTER);
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.pack();
+	}
+
+}
